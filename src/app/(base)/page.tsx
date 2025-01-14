1a463532a041aa60183df4cf502779ec9593d818
bakansm <khuynhbao5@gmail.com>
2025-01-14 11:50:45 +0700

    Update background image

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -62,11 +62,12 @@ export default function Home() {
 
   return (
     <div
-      className={`flex h-[calc(100svh-80px)] flex-col justify-between gap-2 rounded-xl bg-black/60 p-4 bg-blend-overlay`}
+      className={`flex h-[calc(100svh-80px)] flex-col justify-between gap-2 rounded-xl bg-black/60 p-4`}
       style={{
         backgroundImage: `url(${temp})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+        backgroundBlendMode: "soft-light",
       }}>
       <div className="flex items-center space-x-3">
         <SidebarTrigger />
