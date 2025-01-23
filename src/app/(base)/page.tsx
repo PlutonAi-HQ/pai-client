2a1ef9ba4830b206aa12fcb3a76c7640dab1e14a
bakansm <khuynhbao5@gmail.com>
2025-01-23 22:11:10 +0700

    chore: update conversation background shadow

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -8,12 +8,14 @@ import UserInput from "@/components/home/user-input";
 export default function Home() {
   return (
     <div
-      className={`flex h-[calc(100svh-4rem)] flex-col gap-2 rounded-xl p-4`}
+      className={`relative flex h-[calc(100svh-4rem)] flex-col gap-2 rounded-xl p-4`}
       style={{
         backgroundImage: `url(${backgroundImage.src})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+        boxShadow: "inset 0 4px 4px 0 #00000040",
       }}>
+      <div className="absolute left-0 top-0 h-[calc(100svh-4rem)] w-full bg-gradient-to-t from-[#121212] to-transparent" />
       <ConversationMenu />
       <ConversationBox />
       <AgentQuote />
