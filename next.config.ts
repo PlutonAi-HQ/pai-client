39186d81a52cf208c226135e84e06bb0a76ccacf
odin-hoang <uka.pgr@gmail.com>
2025-01-25 20:22:26 +0700

    fix: remove assetPath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  // assetPrefix: "https://plutonai.fun",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
